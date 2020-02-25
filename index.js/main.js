0c58e5f0d3453de6f3382e2487e1c65273ad92dc
Eileen Reid <eileenreid4657@gmail.com>
2020-02-24 19:18:50 -0500

    Working better and better!

---
+++ index.js/main.js
@@ -0,0 +1,82 @@
+const Employee = require("../lib/employee");
+const Engineer = require("../lib/engineer");
+const Manager = require("../lib/manager");
+const Intern = require("../lib/intern");
+const inquirer = require("inquirer");
+const render = require("../lib/htmlRenderer");
+const fs = require("fs");
+const util = require("util");
+var employees = [];
+var writeFileAsync = util.promisify(fs.writeFile);
+var appendFileAsync = util.promisify(fs.appendFile);
+    function promptUser() {
+        return inquirer.prompt([{
+                type: "input",
+                name: "name",
+                message: "What is your employee's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is their id?"
+            },
+            {
+                type: "list",
+                name: "title",
+                choices: ["engineer","manager","intern"],
+                message: "What is their title?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is their email?"
+            },
+            {
+                type: "confirm",
+                name: "repeat",
+                message:"Would you like to add new employee?"
+            }
+            
+        ])
+        .then(answer=> {
+            console.log(answer);
+            employees.push(answer);
+            if(answer.repeat){
+                promptUser()
+            }
+
+            console.log(employees);
+        //     var html = `<html><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        //     <link rel="stylesheet" href="style.css">
+        //     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script></head>
+        //     <body><div class="jumbotron">
+        //     <h1 class="text-center">` + "My Awesome Team!!!" + `</h1>
+        // </div><div class="card">
+        //     <img class="card-img-top" src="/images/pathToYourImage.png" alt="Card image cap">
+        //     <div class="card-body">
+        //       <h4 class="card-title">${answer.name}</h4>
+        //       <p class="card-text">
+        //       This employee is our ${answer.title}, whose Id is ${answer.id}.</n>
+        //       You can contact this person at ${answer.email}. 
+        //       </p>
+              
+        //     </div>
+        //   </div></body></html>`;
+            
+            
+            
+            fs.writeFile("index.html",render(employees),function(err){
+                if(err){
+                    console.log(err);
+                }
+            });a
+            // if(answer.repeat){
+            //     promptUser();
+            // }
+           
+     
+        
+        });
+  
+    }
+promptUser();
--- main.js
+++ main.js
@@ -3,15 +3,14 @@ const Engineer = require("./lib/engineer");
 const Manager = require("./lib/manager");
 const Intern = require("./lib/intern");
 const inquirer = require("inquirer");
-const Renderer = require("./lib/htmlRenderer");
+const render = require("./lib/htmlRenderer");
 const fs = require("fs");
 const util = require("util");
 var employees = [];
 var writeFileAsync = util.promisify(fs.writeFile);
 var appendFileAsync = util.promisify(fs.appendFile);
-
-function ManagerInfo() {
-    return inquirer.prompt([{
+    function managerInfo() {
+        return inquirer.prompt([{
                 type: "input",
                 name: "name",
                 message: "What is your employee's name?"
@@ -21,104 +20,172 @@ function ManagerInfo() {
                 name: "id",
                 message: "What is their id?"
             },
-            {
-                type: "input",
-                name: "officeNumber",
-                message: "What is their Office Number?"
-            },
+            
             {
                 type: "input",
                 name: "email",
                 message: "What is their email?"
             },
             {
-                type: "list",
-                name: "title",
-                choices: ["Engineer", "Intern"],
-                message: "What is their title?"
-
+                type: "input",
+                name: "officeNumber",
+                message: "What is the office number?"
             },
-            {
-                type: "confirm",
-                name: "repeat",
-                message: "Would you like to add new employee?"
-            }
-
+            // {
+            //     type: "confirm",
+            //     name: "repeat",
+            //     message:"Would you like to add new employee?"
+            // }
+            
         ])
-        .then(answer => {
+        .then(answer=> {
             console.log(answer);
-            employees.push(answer);
-            console.log(employees);
 
-        });
+                const manager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+                employees.push(manager);
+            chooseTeam();
+        }) 
+managerInfo();
+
+
+
+function chooseTeam() {
+    inquirer
+      .prompt([
+        {
+            type: "list",
+            name: "title",
+            choices: ["Engineer","Intern", "None"],
+            message: "Assemble your team!!! What is the title?"
+        }
+      ]).then(answer => {
+          switch(answer.title){
+            case "Engineer":
+                engineerInfo();
+                break;
+            case "Intern": 
+                internInfo();
+                break;
+            case "None":
+                console.log(employees);
+                teamEnd();
+                break;
+        }
+    })
 }
 
 function engineerInfo() {
-    if (answer.choices[0] === "Engineer")
-        return inquirer.prompt([{
-                    type: "input",
-                    name: "gitHub",
-                    message: "What is your Git Hub Link?"
-                },
-                
+    inquirer
+    .prompt([{
+        type: "input",
+        name: "name",
+        message: "What is your employee's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is their id?"
+    },
+    
+    {
+        type: "input",
+        name: "email",
+        message: "What is their email?"
+    },
+    
+    {
+        type: "input",
+        name: "gitHub",
+        message: "What is your GitHub Link?"
+    }
 
-            ])
-            .then(answer => {
-                    console.log(answer);
-                    employees.push(answer);
-                    console.log(employees);
-                }
+    ]).then(answer => {
+        console.log(answer)
+        const engineer = new Engineer(answer.name, answer.id, answer.email, answer.officeNumber);
+                employees.push(engineer);
 
-            )
+        chooseTeam();
         
-engineerInfo();   
+    })
+}
 
 function internInfo() {
-    if (answer.choices[1] === "Intern")
-        return inquirer.prompt([{
-                    type: "input",
-                    name: "school",
-                    message: "What is your Alma Mater?"
-                },
+    inquirer
+    .prompt([{
+        type: "input",
+        name: "name",
+        message: "What is your employee's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is their id?"
+    },
+    
+    {
+        type: "input",
+        name: "email",
+        message: "What is their email?"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the office number?"
+    },
+    {
+        type: "input",
+        name: "gitHub",
+        message: "What is your Alma Mater?"
+    }
+
+    ]).then(answer => {
+        console.log(answer);
+
+        const intern = new Intern(answer.name, answer.id, answer.email, answer.officeNumber);
+                employees.push(intern);
+        chooseTeam();
+
+    })
+}
+
+
+            console.log(employees);
+        //     var html = `<html><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        //     <link rel="stylesheet" href="style.css">
+        //     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script></head>
+        //     <body><div class="jumbotron">
+        //     <h1 class="text-center">` + "My Awesome Team!!!" + `</h1>
+        // </div><div class="card">
+        //     <img class="card-img-top" src="/images/pathToYourImage.png" alt="Card image cap">
+        //     <div class="card-body">
+        //       <h4 class="card-title">${answer.name}</h4>
+        //       <p class="card-text">
+        //       This employee is our ${answer.title}, whose Id is ${answer.id}.</n>
+        //       You can contact this person at ${answer.email}. 
+        //       </p>
               
-
-            ])
-            .then(answer => {
-                    console.log(answer);
-                    employees.push(answer);
-                    console.log(employees);
-                }
-
-            )
+        //     </div>
+        //   </div></body></html>`;
+            
+            
+            
+function teamEnd(){
+    fs.writeFile(
+        __dirname + '/Develop/output/team.html', render(employees),
         
-internInfo();   
+        function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Success!");
+        }
+    );
+    };
+      
+           
+     
+        
+        };
+        managerInfo(); 
+    
 
-
-
-
-
-
-
-
-
-fs.writeFile("index.html", html, function (err) {
-    if (err) {
-        console.log(err);
-    }
-    if (answer.repeat) {
-        fs.appendFile("index.html", html, function (err) {
-            if (err) {
-                console.log(err);
-            }
-        });
-        ManagerInfo();
-    }
-
-});
-
-
-
-
-ManagerInfo(employees);
-
-module.exports = Employee
